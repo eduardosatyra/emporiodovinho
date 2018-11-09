7927edd2c8985cd2a016e2751b62f13cde769387
Daywison Ferreira <daywison.leal@gmail.com>
2018-11-09 20:47:16 -0200

    script banco de dados

--- emporiodovinho.sql
+++ emporiodovinho.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Nov-2018 às 04:35
+-- Generation Time: 09-Nov-2018 às 19:56
 -- Versão do servidor: 10.1.36-MariaDB
 -- versão do PHP: 7.2.10
 
@@ -42,7 +42,9 @@ CREATE TABLE `categoria` (
 INSERT INTO `categoria` (`id_categoria`, `nome`, `descricao`, `condicao`) VALUES
 (2, 'Refrigerante', NULL, 1),
 (3, 'Cerveja', NULL, 1),
-(4, 'Vinho', NULL, 1);
+(4, 'Vinho', NULL, 1),
+(5, 'Cachaça', NULL, 1),
+(6, 'Alimentos', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -57,7 +59,7 @@ CREATE TABLE `cliente` (
   `num_doc` varchar(30) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL
+  `status` varchar(255) DEFAULT 'Ativo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -65,11 +67,12 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`id_cliente`, `nome`, `tipo_documento`, `num_doc`, `telefone`, `email`, `status`) VALUES
-(0, 'venda avulsa', 'RG', '22222222', '00000000', NULL, NULL),
-(4, 'Paulo', 'CPF', '36131397821', '46053021', 'paulo@gmail.com', 'Ativo'),
-(5, 'teste', 'CPF', '36131397822', '22221222', 'teste@gmail.com', 'Inativo'),
-(6, 'Joao', 'CPF', '345345435', NULL, NULL, 'Inativo'),
-(7, 'Ayrton Senna', 'RG', '25.112.511-2', '(11) 12121-3123', 'airtonsena@gmail.com', 'Ativo');
+(0, 'venda avulsa', NULL, NULL, NULL, NULL, ''),
+(14, 'Francisco da Silva', 'CPF', '112.312.313-13', '(11) 21321-3122', NULL, 'Ativo'),
+(15, 'Maria das Dores', 'CPF', '556.564.654-65', '(11) 95846-5646', 'maria@gmail.com', 'Ativo'),
+(16, 'Mario de Andrade', 'CPF', '555.887.978-49', '(21) 97874-6546', 'mario@gmail.com', 'Ativo'),
+(17, 'Cristiano da Silva', 'CPF', '555.132.131-23', '(11) 32121-2133', 'cristiano123@gmail.com', 'Ativo'),
+(18, 'Daniela dos Santos', 'CPF', '554.613.131-32', '(11) 22313-2123', 'danielasantos@gmail.com', 'Ativo');
 
 -- --------------------------------------------------------
 
@@ -91,15 +94,30 @@ CREATE TABLE `detalhe_entrada` (
 --
 
 INSERT INTO `detalhe_entrada` (`id_detalhe_entrada`, `id_entrada`, `id_produto`, `quantidade`, `preco_compra`, `preco_venda`) VALUES
-(17, 10, 8, 100, '2.00', '5.00'),
-(18, 10, 10, 200, '2.00', '5.00');
+(36, 35, 14, 200, '5.00', '7.00'),
+(37, 35, 16, 500, '2.50', '5.50'),
+(38, 36, 17, 500, '2.50', '4.50'),
+(39, 37, 15, 500, '2.50', '4.50'),
+(40, 38, 16, 1000, '5.00', '6.00'),
+(41, 39, 18, 200, '2.50', '3.00'),
+(42, 40, 19, 500, '3.60', '4.50'),
+(43, 41, 20, 500, '2.50', '10.00'),
+(44, 42, 21, 1000, '4.00', '6.00'),
+(45, 43, 22, 300, '5.50', '9.00'),
+(46, 44, 14, 500, '4.00', '6.00'),
+(47, 45, 23, 500, '5.00', '8.00'),
+(48, 46, 24, 100, '25.00', '30.00'),
+(49, 47, 25, 200, '20.00', '30.00'),
+(50, 48, 26, 500, '12.00', '30.00'),
+(51, 49, 27, 300, '2.10', '4.00'),
+(52, 50, 28, 200, '5.00', '8.00');
 
 --
 -- Acionadores `detalhe_entrada`
 --
 DELIMITER $$
 CREATE TRIGGER `tr_updEstoqueEntrada` AFTER INSERT ON `detalhe_entrada` FOR EACH ROW BEGIN
-UPDATE produto SET estoque = estoque + NEW.quantidade
+UPDATE produto SET estoque = estoque + NEW.quantidade, preco_compra = NEW.preco_compra, preco_venda = NEW.preco_venda
 WHERE produto.id_produto = NEW.id_produto;
 
 END
@@ -119,21 +137,6 @@ CREATE TABLE `detalhe_saida` (
   `quantidade` int(11) NOT NULL,
   `motivo` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `detalhe_saida`
---
-
-INSERT INTO `detalhe_saida` (`id_detalhe_saida`, `id_saida`, `id_produto`, `quantidade`, `motivo`) VALUES
-(12, 7, 1, 10, 'teste'),
-(13, 8, 3, 10, 'um'),
-(14, 9, 3, 20, 'dadas'),
-(15, 10, 1, 20, 'apenas um teste'),
-(16, 10, 3, 50, 'outro teste'),
-(17, 11, 1, 10, 'TESTE'),
-(18, 12, 3, 2, 'da'),
-(19, 13, 5, 2, 'Quebrou.'),
-(20, 14, 8, 10, 'Quebra de garrafa.');
 
 --
 -- Acionadores `detalhe_saida`
@@ -167,39 +170,21 @@ CREATE TABLE `detalhe_venda` (
 --
 
 INSERT INTO `detalhe_venda` (`id_detalhe_venda`, `id_venda`, `id_produto`, `quantidade`, `preco_venda`, `desconto`) VALUES
-(1, 1, 1, 5, '50.00', '4.00'),
-(2, 2, 2, 10, '30.00', NULL),
-(3, 3, 2, 20, '30.00', NULL),
-(4, 4, 2, 50, '30.00', NULL),
-(5, 5, 1, 10, '30.00', NULL),
-(6, 5, 3, 20, '30.00', NULL),
-(7, 6, 1, 1, '30.00', NULL),
-(8, 6, 2, 1, '30.00', NULL),
-(9, 7, 1, 10, '30.00', '2.00'),
-(10, 8, 3, 10, '30.00', NULL),
-(11, 9, 3, 12, '30.00', NULL),
-(12, 10, 4, 1, '50.00', NULL),
-(13, 11, 4, 10, '50.00', NULL),
-(14, 12, 3, 20, '30.00', '2.00'),
-(15, 12, 1, 2, '30.00', '3.00'),
-(16, 13, 3, 10, '30.00', NULL),
-(17, 15, 2, 5, '30.00', NULL),
-(18, 16, 2, 4, '30.00', NULL),
-(19, 17, 3, 20, '30.00', NULL),
-(20, 18, 2, 8, '30.00', NULL),
-(21, 20, 4, 2, '50.00', '0.00'),
-(22, 21, 3, 5, '30.00', '0.00'),
-(23, 22, 4, 12, '50.00', '0.00'),
-(24, 23, 4, 2, '50.00', '0.00'),
-(25, 24, 4, 80, '50.00', '0.00'),
-(26, 24, 4, 80, '50.00', '0.00'),
-(27, 25, 5, 1, '29.99', NULL),
-(28, 26, 3, 20, '30.00', NULL),
-(29, 27, 9, 10, '6.00', NULL),
-(30, 28, 9, 20, '6.00', NULL),
-(31, 29, 10, 90, '3.00', NULL),
-(32, 30, 12, 20, '60.00', NULL),
-(33, 31, 12, 20, '60.00', NULL);
+(46, 51, 14, 100, '6.00', '0.00'),
+(47, 52, 15, 200, '4.50', '0.00'),
+(48, 53, 15, 100, '4.50', '0.00'),
+(49, 54, 14, 200, '6.00', '0.00'),
+(50, 55, 14, 133, '6.00', '0.00'),
+(51, 56, 19, 200, '4.50', '0.00'),
+(52, 57, 21, 210, '6.00', '0.00'),
+(53, 58, 23, 200, '8.00', '0.00'),
+(54, 59, 20, 100, '10.00', '0.00'),
+(55, 60, 21, 150, '6.00', '0.00'),
+(56, 61, 22, 100, '9.00', '0.00'),
+(57, 62, 14, 266, '6.00', '0.00'),
+(58, 63, 19, 150, '4.50', '0.00'),
+(59, 64, 23, 111, '8.00', '0.00'),
+(60, 65, 15, 2, '4.50', '0.00');
 
 --
 -- Acionadores `detalhe_venda`
@@ -221,15 +206,31 @@ CREATE TABLE `entrada` (
   `id_entrada` int(11) NOT NULL,
   `id_fornecedor` int(11) NOT NULL,
   `tipo_pagamento` varchar(45) DEFAULT NULL,
-  `data_hora` datetime NOT NULL
+  `data_hora` datetime NOT NULL,
+  `total_entrada` decimal(9,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `entrada`
 --
 
-INSERT INTO `entrada` (`id_entrada`, `id_fornecedor`, `tipo_pagamento`, `data_hora`) VALUES
-(10, 11, 'dinheiro', '2018-11-08 00:53:53');
+INSERT INTO `entrada` (`id_entrada`, `id_fornecedor`, `tipo_pagamento`, `data_hora`, `total_entrada`) VALUES
+(35, 13, 'dinheiro', '2018-10-09 15:52:50', '2250.00'),
+(36, 14, 'dinheiro', '2018-10-09 15:53:27', '1250.00'),
+(37, 15, 'dinheiro', '2018-09-09 15:53:48', '1250.00'),
+(38, 15, 'dinheiro', '2018-09-09 15:54:11', '5000.00'),
+(39, 16, 'dinheiro', '2018-11-09 15:54:32', '500.00'),
+(40, 13, 'dinheiro', '2018-11-09 15:54:49', '1800.00'),
+(41, 15, 'dinheiro', '2018-11-09 15:55:18', '1250.00'),
+(42, 13, 'dinheiro', '2018-11-09 15:55:36', '4000.00'),
+(43, 13, 'dinheiro', '2018-11-09 15:55:54', '1650.00'),
+(44, 15, 'dinheiro', '2018-11-09 15:56:39', '2000.00'),
+(45, 13, 'dinheiro', '2018-11-09 15:57:04', '2500.00'),
+(46, 13, 'dinheiro', '2018-11-09 16:06:44', '2500.00'),
+(47, 13, 'dinheiro', '2018-11-09 16:07:03', '4000.00'),
+(48, 13, 'dinheiro', '2018-11-09 16:07:23', '6000.00'),
+(49, 13, 'dinheiro', '2018-11-09 16:07:50', '630.00'),
+(50, 13, 'dinheiro', '2018-11-09 16:09:11', '1000.00');
 
 -- --------------------------------------------------------
 
@@ -259,12 +260,10 @@ CREATE TABLE `fornecedor` (
 --
 
 INSERT INTO `fornecedor` (`id_fornecedor`, `nome`, `tipo_documento`, `num_doc`, `endereco`, `cep`, `numero`, `bairro`, `cidade`, `estado`, `complemento`, `telefone`, `email`, `status`) VALUES
-(4, 'george', 'CPF', '36131397822', 'Rua das Flores', NULL, NULL, NULL, NULL, NULL, NULL, '46053021', 'george@gmail.com', 'Inativo'),
-(5, 'Lucas', 'CPF', '1231313131', 'Rua do pente', NULL, NULL, NULL, NULL, NULL, NULL, '222123112', 'lucas@gmail.com', 'Inativo'),
-(6, 'fornecedor teste', 'CPF', NULL, 'rua teste', '07700000', '11', 'teste', 'caieiras', 'sp', NULL, '123123123', 'dasdada@dadasd.com', 'Inativo'),
-(9, 'dasdasd', 'CPF', '2332423423', 'dsadasd', '1231231', '1231', 'dasdasd', 'dasdas', 'dasds', NULL, '12312312', 'dasdada@dasdasd', 'Inativo'),
-(10, 'apenas um teste', 'CPF', '312312312', 'dsadada', '12312312', '1231', 'dadssad', 'dasdasd', 'dasdasd', 'dasd', '3123123', 'dadada@dadad.com', 'Inativo'),
-(11, 'juca', 'CPF', '361.313.987-89', 'dada', '54654-654', '11', 'dasjdaksdj', 'caieiras', 'SP', NULL, '(13) 13123-1231', 'dadada@dadas.com', 'Ativo');
+(13, 'Francisco Teixeira', 'CPF', '556.541.321-32', 'Rua São Francisco', '08753-231', '55', 'Tiradentes', 'Tiradentes', 'SP', NULL, '(11) 56451-2133', NULL, 'Ativo'),
+(14, 'Pedro Miguel', 'CPF', '884.641.313-21', 'Rua São Miguel', '07713-010', '55', 'Mooca', 'São Paulo', 'SP', NULL, '(55) 13133-1321', 'pedro@gmail.com', 'Ativo'),
+(15, 'Paula da Silva Oliveira', 'CPF', '551.313.213-21', 'Rua das flores', '07613-123', '554', 'Serpa', 'Caieiras', 'SP', NULL, '(55) 13212-3132', 'paulasilva@hotmail.com', 'Ativo'),
+(16, 'Teobaldo Flores', 'CPF', '223.213.213-21', 'rua santa barbara', '05513-210', '54', 'Vila ramos', 'Jundiai', 'SP', NULL, '(22) 51312-3123', 'teobaldo@gmail.com', 'Ativo');
 
 -- --------------------------------------------------------
 
@@ -289,7 +288,7 @@ CREATE TABLE `produto` (
   `id_categoria` int(11) NOT NULL,
   `codigo` varchar(50) DEFAULT NULL,
   `nome` varchar(100) NOT NULL,
-  `estoque` int(11) NOT NULL,
+  `estoque` int(11) NOT NULL DEFAULT '0',
   `estoque_minimo` int(11) NOT NULL,
   `preco_compra` decimal(9,2) DEFAULT NULL,
   `preco_venda` decimal(9,2) DEFAULT NULL,
@@ -302,11 +301,21 @@ CREATE TABLE `produto` (
 --
 
 INSERT INTO `produto` (`id_produto`, `id_categoria`, `codigo`, `nome`, `estoque`, `estoque_minimo`, `preco_compra`, `preco_venda`, `descricao`, `status`) VALUES
-(8, 2, '010', 'Coca-Cola 600ml', 190, 10, '3.00', '5.00', NULL, 'Ativo'),
-(9, 3, '020', 'Cerveja Artarctica Original 600ml', 70, 10, '4.00', '6.00', NULL, 'Ativo'),
-(10, 3, '100', 'Cerveja Skol 269ml', 310, 15, '2.00', '3.00', NULL, 'Ativo'),
-(11, 4, '122', 'Vinho Santa Carolina Reservado Cabernet Sauvignon - 750ml.', 300, 20, '20.00', '40.00', NULL, 'Ativo'),
-(12, 4, '200', 'Vinho Olaria Tinto Suave 750ml.', 14, 15, '40.00', '60.00', NULL, 'Ativo');
+(14, 3, '01', 'Cerveja skol 269ml', 1, 10, '4.00', '6.00', NULL, 'Ativo'),
+(15, 3, '02', 'Cerveja Itaipava 269ml', 198, 10, '2.50', '4.50', NULL, 'Ativo'),
+(16, 3, '03', 'Cerveja brahma 600ml', 1500, 10, '5.00', '6.00', NULL, 'Ativo'),
+(17, 3, '04', 'Cerveja heineken 250ml', 500, 10, '2.50', '4.50', NULL, 'Ativo'),
+(18, 2, '010', 'Refrigerante coca cola 600ml', 200, 10, '2.50', '3.00', NULL, 'Ativo'),
+(19, 2, '020', 'Refrigerante pepsi cola 600ml', 150, 10, '3.60', '4.50', NULL, 'Ativo'),
+(20, 6, '021', 'Queijo meia cura', 400, 10, '2.50', '10.00', NULL, 'Ativo'),
+(21, 5, '033', 'Cachaça lua cheia 600ml', 640, 5, '4.00', '6.00', NULL, 'Ativo'),
+(22, 4, '050', 'Vinho JP Chenet Cabernet Syrah', 200, 5, '5.50', '9.00', NULL, 'Ativo'),
+(23, 4, '077', 'Vinho Piscine Rose', 189, 10, '5.00', '8.00', NULL, 'Ativo'),
+(24, 4, '088', 'Vinho do Porto Ferreira Ruby 750ml', 100, 10, '25.00', '30.00', NULL, 'Ativo'),
+(25, 4, '099', 'Vinho Chilano Cabernet Sauvignon', 200, 15, '20.00', '30.00', NULL, 'Ativo'),
+(26, 2, '101', 'Vinho Yellow Tail Shiraz', 500, 15, '12.00', '30.00', NULL, 'Ativo'),
+(27, 3, '105', 'Cerveja Corona Extra Long Neck 355ml', 300, 20, '2.10', '4.00', NULL, 'Ativo'),
+(28, 3, '401', 'Cerveja Therezopolis Gold 355ml', 200, 10, '5.00', '8.00', NULL, 'Ativo');
 
 -- --------------------------------------------------------
 
@@ -319,13 +328,6 @@ CREATE TABLE `saida` (
   `id_usuario` int(11) NOT NULL,
   `data_hora` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `saida`
---
-
-INSERT INTO `saida` (`id_saida`, `id_usuario`, `data_hora`) VALUES
-(14, 1, '2018-11-08 01:01:09');
 
 -- --------------------------------------------------------
 
@@ -348,7 +350,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2y$10$3q5tSo4hTbxiClTVBpjeee1a/EOmpRzj6OsISnyvkRcAbM4DR111a', 'U1tGNAJ6H51hXOn8m16sC4TQllB8QNpjW1OE9Fbo6N5izM5dSSWgB3OeXZRm', '2018-09-01 00:41:39', '2018-09-01 00:41:39'),
+(1, 'admin', 'admin@gmail.com', '$2y$10$3q5tSo4hTbxiClTVBpjeee1a/EOmpRzj6OsISnyvkRcAbM4DR111a', 'ehXniNlaZxVginSNRhnwRyAinIChzBSYbey3OaqdI29UfcEKbh4WNceaKIdQ', '2018-09-01 00:41:39', '2018-09-01 00:41:39'),
 (3, 'paula', 'paula@gmail.com', '$2y$10$YAoQ6izbSLIIJF70ZmMMA.Vop0.3JLZvTPHrJuXWyFQriCi12tcGe', NULL, '2018-09-01 15:21:10', '2018-09-01 15:34:21');
 
 -- --------------------------------------------------------
@@ -370,10 +372,21 @@ CREATE TABLE `venda` (
 --
 
 INSERT INTO `venda` (`id_venda`, `id_cliente`, `tipo_pagamento`, `data_hora`, `total_venda`) VALUES
-(27, 0, 'dinheiro', '2018-11-08 00:01:30', '60.00'),
-(28, 4, 'dinheiro', '2018-11-08 00:03:13', '120.00'),
-(29, 0, 'dinheiro', '2018-11-08 00:04:14', '270.00'),
-(31, 0, 'dinheiro', '2018-11-08 00:14:34', '1200.00');
+(51, 0, 'dinheiro', '2018-10-09 16:00:51', '600.00'),
+(52, 0, 'dinheiro', '2018-11-09 16:01:22', '900.00'),
+(53, 14, 'dinheiro', '2018-11-09 16:01:45', '450.00'),
+(54, 0, 'dinheiro', '2018-09-09 16:02:07', '1200.00'),
+(55, 0, 'dinheiro', '2018-09-09 16:02:29', '798.00'),
+(56, 0, 'dinheiro', '2018-09-09 16:02:48', '900.00'),
+(57, 0, 'dinheiro', '2018-11-09 16:03:09', '1260.00'),
+(58, 0, 'dinheiro', '2018-11-09 16:03:29', '1600.00'),
+(59, 0, 'dinheiro', '2018-11-09 16:03:51', '1000.00'),
+(60, 0, 'dinheiro', '2018-11-09 16:04:13', '900.00'),
+(61, 0, 'dinheiro', '2018-11-09 16:04:48', '900.00'),
+(62, 0, 'dinheiro', '2018-11-09 16:09:50', '1596.00'),
+(63, 0, 'dinheiro', '2018-11-09 16:18:11', '675.00'),
+(64, 0, 'dinheiro', '2018-11-09 16:18:34', '888.00'),
+(65, 0, 'dinheiro', '2018-11-09 16:19:19', '9.00');
 
 --
 -- Indexes for dumped tables
@@ -469,43 +482,43 @@ ALTER TABLE `venda`
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `detalhe_entrada`
 --
 ALTER TABLE `detalhe_entrada`
-  MODIFY `id_detalhe_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_detalhe_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `detalhe_saida`
 --
 ALTER TABLE `detalhe_saida`
-  MODIFY `id_detalhe_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_detalhe_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `detalhe_venda`
 --
 ALTER TABLE `detalhe_venda`
-  MODIFY `id_detalhe_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_detalhe_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `entrada`
 --
 ALTER TABLE `entrada`
-  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `fornecedor`
 --
 ALTER TABLE `fornecedor`
-  MODIFY `id_fornecedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_fornecedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -517,13 +530,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `saida`
 --
 ALTER TABLE `saida`
-  MODIFY `id_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -535,7 +548,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `venda`
 --
 ALTER TABLE `venda`
-  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- Constraints for dumped tables
